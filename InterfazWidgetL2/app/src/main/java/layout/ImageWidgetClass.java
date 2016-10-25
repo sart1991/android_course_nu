b0fc08e9de8cbac3ac290fab30c26c7cfc310cce
Sergio Rojas <sergiorojas@qdata.io>
2016-10-25 17:43:03 -0500

    InterfazWidgetL3

---
+++ InterfazWidgetL2/app/src/main/java/layout/ImageWidgetClass.java
@@ -0,0 +1,17 @@
+package layout;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * Created by Admin on 10/25/2016.
+ */
+
+public class ImageWidgetClass extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+    }
+}
