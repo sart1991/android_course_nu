c872dcee599def6715a2a1b0bc44a7a3d4e3aec6
Sergio Rojas <sergiorojas@qdata.io>
2016-10-25 18:42:34 -0500

    WidgetHerramientaL5- analog clock does not work

---
+++ WidgetHerramientaL5/app/src/main/java/layout/WidgetNextU.java
@@ -0,0 +1,21 @@
+package layout;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+import com.exercise.nextu.widgetherramiental5.R;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class WidgetNextU extends AppWidgetProvider {
+
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+    }
+}
+
