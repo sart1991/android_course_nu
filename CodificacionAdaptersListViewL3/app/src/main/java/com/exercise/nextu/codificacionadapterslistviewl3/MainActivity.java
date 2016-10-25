49b5180550fcd5e6ef453d362437563d91192426
Sergio Rojas <sart1991@gmail.com>
2016-10-24 20:08:41 -0500

    CodificacionAdaptersListViewL3- button alarm updated

--- CodificacionAdaptersListViewL3/app/src/main/java/com/exercise/nextu/codificacionadapterslistviewl3/MainActivity.java
+++ CodificacionAdaptersListViewL3/app/src/main/java/com/exercise/nextu/codificacionadapterslistviewl3/MainActivity.java
@@ -1,13 +1,24 @@
 package com.exercise.nextu.codificacionadapterslistviewl3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        intent = new Intent();
+    }
+
+    public void onAlarmaButtonClicked(View view) {
+        intent.setClass(this, AlarmaActivity.class);
+        startActivity(intent);
     }
 }
