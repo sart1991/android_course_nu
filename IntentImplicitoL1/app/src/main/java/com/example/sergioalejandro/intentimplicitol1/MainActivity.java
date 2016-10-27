caf3ae6fcd4e5a91f36c42a24461ccafe20cc6cd
Sergio Rojas <sart1991@gmail.com>
2016-10-26 20:50:50 -0500

    IntentImplicictoL1- web search intent

--- IntentImplicitoL1/app/src/main/java/com/example/sergioalejandro/intentimplicitol1/MainActivity.java
+++ IntentImplicitoL1/app/src/main/java/com/example/sergioalejandro/intentimplicitol1/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.sergioalejandro.intentimplicitol1;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+        startActivity(intent);
     }
 }
