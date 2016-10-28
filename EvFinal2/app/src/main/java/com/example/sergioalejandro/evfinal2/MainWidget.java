bbf603768e54f331e4b388288b3a8c0c66b0619d
Sergio Rojas <sergiorojas@qdata.io>
2016-10-27 19:07:58 -0500

    EvFinal2 - step 5 done

---
+++ EvFinal2/app/src/main/java/com/example/sergioalejandro/evfinal2/MainWidget.java
@@ -0,0 +1,18 @@
+package com.example.sergioalejandro.evfinal2;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class MainWidget extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+    }
+}
+
