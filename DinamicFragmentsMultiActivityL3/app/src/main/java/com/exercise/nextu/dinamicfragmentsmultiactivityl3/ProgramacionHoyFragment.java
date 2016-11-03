4aea6a171d003fdf680179413304f53d91007d1a
Sergio Rojas <sergiorojas@qdata.io>
2016-11-03 16:46:10 -0500

    DinamicFragmentsMultiActivityL4 - First commit

---
+++ DinamicFragmentsMultiActivityL3/app/src/main/java/com/exercise/nextu/dinamicfragmentsmultiactivityl3/ProgramacionHoyFragment.java
@@ -0,0 +1,28 @@
+package com.exercise.nextu.dinamicfragmentsmultiactivityl3;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/**
+ * Created by Admin on 11/3/2016.
+ */
+
+public class ProgramacionHoyFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_programacion_hoy, container, false);
+        String[] stringArray = getResources().getStringArray(R.array.programacion_hoy);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, stringArray);
+        ListView listView = (ListView) view.findViewById(R.id.list_view_programacion_hoy);
+        listView.setAdapter(arrayAdapter);
+        return view;
+    }
+}
