4abcba9c49dfd4151b2926ca02d0d2b2d43268b7
Sergio Rojas <sart1991@gmail.com>
2016-11-07 23:00:09 -0500

    EvaluacionFinal3 - second commit

---
+++ EvaluacionFinal3/app/src/main/java/com/example/sergioalejandro/evaluacionfinal3/resources/InstrumentsClasification.java
@@ -0,0 +1,9 @@
+package com.example.sergioalejandro.evaluacionfinal3.resources;
+
+/**
+ * Created by SergioAlejandro on 7/11/2016.
+ */
+
+public enum InstrumentsClasification {
+    CUERDAS, PERCUSION, VIENTO, ELECTRICOS;
+}
