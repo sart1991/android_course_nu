17a85a2d90d1eba500881d8fbae5bf76b5372e76
Sergio Rojas <sergiorojas@qdata.io>
2016-11-17 19:25:07 -0500

    Evaluacion Final 3 - modificaciones resultados 1

--- EvaluacionFinal3/app/src/main/java/com/example/sergioalejandro/evaluacionfinal3/MainActivity.java
+++ EvaluacionFinal3/app/src/main/java/com/example/sergioalejandro/evaluacionfinal3/MainActivity.java
@@ -6,15 +6,18 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.sergioalejandro.evaluacionfinal3.communication.ICommunication;
 import com.example.sergioalejandro.evaluacionfinal3.model.Instrument;
 import com.example.sergioalejandro.evaluacionfinal3.service.InstrumentsService;
 
-public class MainActivity extends AppCompatActivity implements ICommunication.IInstrumentsList, ICommunication.IInstrumentsDetails{
+public class MainActivity extends AppCompatActivity implements ICommunication.IInstrumentsList, ICommunication.IInstrumentsDetails, View.OnClickListener{
 
     private int position = 0;
     private View view;
+    private Instrument instrument1;
+    private Instrument instrument2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,15 +51,40 @@ public class MainActivity extends AppCompatActivity implements ICommunication.II
         view = v;
         InstrumentsService.setContext(this);
         ((TextView)v.findViewById(R.id.text_clasification_description)).setText(InstrumentsService.getClasificationContent(position));
-        Instrument instrument1 = InstrumentsService.getClasificationInstruments(position).get(0);
-        Instrument instrument2 = InstrumentsService.getClasificationInstruments(position).get(1);
+        instrument1 = InstrumentsService.getClasificationInstruments(position).get(0);
+        instrument2 = InstrumentsService.getClasificationInstruments(position).get(1);
         ((ImageView)v.findViewById(R.id.details_image_view_content_1)).setImageResource(instrument1.getImageSrc());
         ((ImageView)v.findViewById(R.id.details_image_view_content_2)).setImageResource(instrument2.getImageSrc());
+        ImageButton btnFavorite1 = (ImageButton) v.findViewById(R.id.image_btn_favorite_1);
+        ImageButton btnFavorite2 = (ImageButton) v.findViewById(R.id.image_btn_favorite_2);
         if (instrument1.isFavorite()){
-            ((ImageButton)v.findViewById(R.id.image_btn_favorite_1)).setImageResource(R.drawable.ic_star_accent_24dp);
+            modifyIcon(btnFavorite1);
         }
         if (instrument2.isFavorite()){
-            ((ImageButton)v.findViewById(R.id.image_btn_favorite_2)).setImageResource(R.drawable.ic_star_accent_24dp);
+            modifyIcon(btnFavorite2);
+        }
+        btnFavorite1.setOnClickListener(this);
+        btnFavorite2.setOnClickListener(this);
+        v.findViewById(R.id.image_btn_play_1).setOnClickListener(this);
+        v.findViewById(R.id.image_btn_play_2).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.image_btn_favorite_1:
+                togleFavorite(instrument1);
+                modifyIcon((ImageButton)view);
+                generatePersonalizedToast("Instrumento: " + instrument1.getName() + " es ahora favorito.");
+                break;
+            case R.id.image_btn_play_1:
+                break;
+            case R.id.image_btn_favorite_2:
+                togleFavorite(instrument2);
+                modifyIcon((ImageButton)view);
+                break;
+            case R.id.image_btn_play_2:
+                break;
         }
     }
 
@@ -64,5 +92,17 @@ public class MainActivity extends AppCompatActivity implements ICommunication.II
     public void onBackPressed() {
         this.setTitle(R.string.app_name);
         super.onBackPressed();
+    }
+
+    private void modifyIcon(ImageButton imageButton) {
+        imageButton.setImageResource(R.drawable.ic_star_accent_24dp);
+    }
+
+    private void togleFavorite(Instrument instrument) {
+        instrument.setFavorite(!instrument.isFavorite());
+    }
+
+    private void generatePersonalizedToast(String string) {
+        Toast.makeText(this, string, Toast.LENGTH_LONG).show();
     }
 }
