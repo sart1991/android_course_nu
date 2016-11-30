c1ae64891152c4bcf2e0f84140e447c16aa9cbd1
Sergio Rojas <sergiorojas@qdata.io>
2016-11-30 18:01:10 -0500

    CanvasExercisesU4E3 - figures canvas

--- CanvasExercisesU4W3/app/src/main/java/com/exercise/nextu/canvasexercisesu4w3/MainActivity.java
+++ CanvasExercisesU4W3/app/src/main/java/com/exercise/nextu/canvasexercisesu4w3/MainActivity.java
@@ -1,13 +1,44 @@
 package com.exercise.nextu.canvasexercisesu4w3;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Paper paper = new Paper(this);
+        setContentView(paper);
+    }
+}
+
+class Paper extends View {
+
+    private Context context;
+
+    public Paper(Context context) {
+        super(context);
+        this.context = context;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        Paint paint = new Paint();
+        paint.setColor(Color.BLUE);
+        canvas.drawPaint(paint);
+
+
+
+        Toast.makeText(context, "Canvas", Toast.LENGTH_LONG).show();
+
     }
 }
