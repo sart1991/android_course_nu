d4751f66528332de88822bba1875aee5a02e9d4d
Sergio Rojas <sergiorojas@qdata.io>
2016-12-02 17:41:50 -0500

    CanvasExercisesU4E3 - app finished

--- CanvasExercisesU4W3/app/src/main/java/com/exercise/nextu/canvasexercisesu4w3/MainActivity.java
+++ CanvasExercisesU4W3/app/src/main/java/com/exercise/nextu/canvasexercisesu4w3/MainActivity.java
@@ -1,6 +1,8 @@
 package com.exercise.nextu.canvasexercisesu4w3;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.DashPathEffect;
@@ -8,6 +10,7 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Toast;
 
@@ -24,10 +27,17 @@ public class MainActivity extends AppCompatActivity {
 class Paper extends View {
 
     private Context context;
+    private float x;
+    private float y;
+    private int id;
+    private Path dPath = new Path();
+    private Bitmap bitmap;
+
 
     public Paper(Context context) {
         super(context);
         this.context = context;
+        bitmap =  BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
     }
 
     @Override
@@ -62,5 +72,34 @@ class Paper extends View {
         path.offset(0, 40);
 
         canvas.drawPath(path, paint);
+
+        paint.setStrokeWidth(4);
+        paint.setColor(Color.RED);
+
+
+        canvas.drawBitmap(bitmap, 0, 0, paint);
+        if (id == 1) {
+            dPath.moveTo(x, y);
+        } else if (id == 2) {
+            dPath.lineTo(x, y);
+        }
+        canvas.drawPath(dPath, paint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        x = event.getX();
+        y = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                id = 1;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                id = 2;
+                break;
+        }
+        invalidate();
+        return true;
     }
 }
