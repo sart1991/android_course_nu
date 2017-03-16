2fdf1240af4c13b24f69f117e0847587980a7d61
Sergio Rojas <sart1991@gmail.com>
2017-03-15 22:06:00 -0500

    first commit toolbar

--- WebViewW1M1/app/src/main/java/com/exercises/sart1991/webvieww1m1/MainActivity.java
+++ WebViewW1M1/app/src/main/java/com/exercises/sart1991/webvieww1m1/MainActivity.java
@@ -1,13 +1,56 @@
 package com.exercises.sart1991.webvieww1m1;
 
+import android.net.Uri;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView mWebView;
+    ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.my_web_view);
+        mProgressBar = (ProgressBar) findViewById(R.id.my_progress_bar);
+
+        String url = "http://www.google.com";
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient());
+        setTitle(url);
+        mWebView.loadUrl(url);
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                mProgressBar.setProgress(0);
+                mProgressBar.setVisibility(View.VISIBLE);
+                mProgressBar.incrementProgressBy(newProgress);
+
+                if (newProgress > 98) {
+                    mProgressBar.setVisibility(View.GONE);
+                }
+            }
+        });
+    }
+
+    public void onClickNavigate(View view) {
+        String url = "http://www.google.com";
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        builder.setToolbarColor(ContextCompat.getColor(this, R.color.colorPrimary));
+        builder.setStartAnimations(this, android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+        builder.setExitAnimations(this, android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+        CustomTabsIntent tabsIntent = builder.build();
+        tabsIntent.launchUrl(this, Uri.parse(url));
     }
 }
