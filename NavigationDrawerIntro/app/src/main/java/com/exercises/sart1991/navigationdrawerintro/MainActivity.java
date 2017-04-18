24336b03567ebf04c6f36aa69e8b263072a17720
Sergio Rojas <sart1991@gmail.com>
2017-04-18 17:04:25 -0500

    navigation drawer working but empty

--- NavigationDrawerIntro/app/src/main/java/com/exercises/sart1991/navigationdrawerintro/MainActivity.java
+++ NavigationDrawerIntro/app/src/main/java/com/exercises/sart1991/navigationdrawerintro/MainActivity.java
@@ -1,13 +1,31 @@
 package com.exercises.sart1991.navigationdrawerintro;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout_main);
+
+        ActionBarDrawerToggle drawerToggle =
+                new ActionBarDrawerToggle(
+                        this, drawerLayout,
+                        toolbar, R.string.open_drawer,
+                        R.string.close_drawer);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
     }
 }
