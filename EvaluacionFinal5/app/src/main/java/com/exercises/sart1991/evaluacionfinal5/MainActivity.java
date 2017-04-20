82e9291c206fea717dcb034f3c696600fd92612d
Sergio Rojas <sart1991@gmail.com>
2017-04-19 23:03:05 -0500

    test for new nav drawer and tab layout implementation

--- EvaluacionFinal5/app/src/main/java/com/exercises/sart1991/evaluacionfinal5/MainActivity.java
+++ EvaluacionFinal5/app/src/main/java/com/exercises/sart1991/evaluacionfinal5/MainActivity.java
@@ -1,13 +1,54 @@
 package com.exercises.sart1991.evaluacionfinal5;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.exercises.sart1991.evaluacionfinal5.usable.CustomTheme;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CustomTheme.onActivityCreateSetTheme(this);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_main);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        int theme = R.style.AppTheme;
+        switch (itemId) {
+            case R.id.drawer_facebook:
+                theme = R.style.AppTheme_Facebook;
+                break;
+            case R.id.drawer_instagram:
+                theme = R.style.AppTheme_Instagram;
+                break;
+            case R.id.drawer_gplus:
+                theme = R.style.AppTheme_Gplus;
+                break;
+            case R.id.drawer_twitter:
+                theme = R.style.AppTheme_Twitter;
+                break;
+        }
+        CustomTheme.changeToTheme(this, theme);
+        return true;
     }
 }
---
+++ EvaluacionFinal5/app/src/main/java/com/exercises/sart1991/evaluacionfinal5/usable/CustomTheme.java
@@ -0,0 +1,51 @@
+package com.exercises.sart1991.evaluacionfinal5.usable;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import com.exercises.sart1991.evaluacionfinal5.R;
+
+/**
+ * Created by sart1 on 4/19/2017.
+ */
+
+public class CustomTheme {
+    private static int sTheme;
+    public final static int THEME_DEFAULT = 0;
+    public final static int THEME_FACEBOOK = 1;
+    public final static int THEME_INSTAGRAM = 2;
+    public final static int THEME_GPLUS = 3;
+    public final static int THEME_TWITTER = 4;
+    /**
+     * Set the theme of the Activity, and restart it by creating a new Activity of the same type.
+     */
+    public static void changeToTheme(Activity activity, int theme)
+    {
+        sTheme = theme;
+        activity.finish();
+        activity.startActivity(new Intent(activity, activity.getClass()));
+    }
+    /** Set the theme of the activity, according to the configuration. */
+    public static void onActivityCreateSetTheme(Activity activity)
+    {
+        switch (sTheme)
+        {
+            default:
+            case THEME_DEFAULT:
+                activity.setTheme(R.style.AppTheme);
+                break;
+            case THEME_FACEBOOK:
+                activity.setTheme(R.style.AppTheme_Facebook);
+                break;
+            case THEME_INSTAGRAM:
+                activity.setTheme(R.style.AppTheme_Instagram);
+                break;
+            case THEME_GPLUS:
+                activity.setTheme(R.style.AppTheme_Gplus);
+                break;
+            case THEME_TWITTER:
+                activity.setTheme(R.style.AppTheme_Twitter);
+                break;
+        }
+    }
+}
