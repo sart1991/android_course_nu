e74e76ca7ae3bc731f0e2764e9cb5b266f5593d7
Sergio Rojas <sergio@monolegal.co>
2017-10-05 20:38:22 -0500

    geofences activity progress

--- EvaluacionFinal11/app/src/main/java/com/exercises/sart1991/evaluacionfinal11/repository/geofence/GeofenceTransitionIntentService.java
+++ EvaluacionFinal11/app/src/main/java/com/exercises/sart1991/evaluacionfinal11/repository/geofence/GeofenceTransitionIntentService.java
@@ -6,9 +6,14 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+import com.exercises.sart1991.evaluacionfinal11.utils.EVConstants;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingEvent;
 import com.google.android.gms.location.LocationServices;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by sart1 on 9/29/2017.
@@ -35,7 +40,28 @@ public class GeofenceTransitionIntentService extends IntentService implements
 
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
+        GeofencingEvent geoEvent = GeofencingEvent.fromIntent(intent);
+        if (geoEvent.hasError()) {
+            //TODO: Show error
+        } else  {
+            int transitionType = geoEvent.getGeofenceTransition();
+            if (Geofence.GEOFENCE_TRANSITION_ENTER == transitionType) {
+                mGoogleApiClient.blockingConnect(EVConstants.CONNECTION_TIME_OUT_MS,
+                                                 TimeUnit.MICROSECONDS);
+                String triggerGeofence = geoEvent.getTriggeringGeofences().get(0).getRequestId();
 
+                //TODO: mostrar mensaje entrada al usuario
+
+                mGoogleApiClient.disconnect();
+            } else if (Geofence.GEOFENCE_TRANSITION_EXIT == transitionType) {
+                mGoogleApiClient.blockingConnect(EVConstants.CONNECTION_TIME_OUT_MS,
+                        TimeUnit.MICROSECONDS);
+
+                //TODO: mostrar mensaje salida al usuario
+
+                mGoogleApiClient.disconnect();
+            }
+        }
     }
 
     @Override
--- EvaluacionFinal11/app/src/main/java/com/exercises/sart1991/evaluacionfinal11/ui/activities/ContentActivity.java
+++ EvaluacionFinal11/app/src/main/java/com/exercises/sart1991/evaluacionfinal11/ui/activities/ContentActivity.java
@@ -1,10 +1,13 @@
 package com.exercises.sart1991.evaluacionfinal11.ui.activities;
 
 import android.Manifest;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BaseTransientBottomBar;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -16,11 +19,20 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.exercises.sart1991.evaluacionfinal11.R;
+import com.exercises.sart1991.evaluacionfinal11.repository.geofence.GeofenceTransitionIntentService;
 import com.exercises.sart1991.evaluacionfinal11.utils.EVConstants;
 import com.facebook.Profile;
 import com.facebook.ProfileTracker;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.LocationServices;
 
-public class ContentActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContentActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private static final String TAG = ContentActivity.class.getSimpleName();
 
@@ -29,6 +41,11 @@ public class ContentActivity extends AppCompatActivity {
 
     private SharedPreferences preferences;
     private ProfileTracker profileTracker;
+
+    private Geofence mAndroidGeofence;
+    private List<Geofence> geofences;
+    private PendingIntent mGeofenceRequestIntent;
+    private GoogleApiClient mGoogleApiCleint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +57,8 @@ public class ContentActivity extends AppCompatActivity {
     private void setUp() {
         preferences = PreferenceManager.getDefaultSharedPreferences(this);
         bindViews();
-        requestPermissions();
+        //requestPermissions();
+
         fab.setOnClickListener(fabListener);
         profileTracker = new ProfileTracker() {
             @Override
@@ -76,6 +94,30 @@ public class ContentActivity extends AppCompatActivity {
         }
     }
 
+    private void createGoogleApiClient() {
+        if (!availableGoogleApiServies()) {
+            return;
+        }
+        mGoogleApiCleint = new GoogleApiClient.Builder(this).addApi(LocationServices.API)
+                                                            .addConnectionCallbacks(this)
+                                                            .addOnConnectionFailedListener(this)
+                                                            .build();
+        mGoogleApiCleint.connect();
+        geofences = new ArrayList<>();
+
+    }
+
+    public void createGeofences() {
+
+        Geofence geofence = new Geofence.Builder()
+                .setRequestId(EVConstants.ANDROID_ID)
+                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER | Geofence.GEOFENCE_TRANSITION_EXIT)
+                .setCircularRegion(EVConstants.ANDROID_LATITUDE, EVConstants.ANDROID_LONGITUDE, EVConstants.ANDROID_RADIUS)
+                .setExpirationDuration(Geofence.NEVER_EXPIRE)
+                .build();
+        geofences.add(geofence);
+    }
+
     private FloatingActionButton.OnClickListener fabListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
@@ -91,5 +133,39 @@ public class ContentActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         profileTracker.stopTracking();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        requestPermissions();
+        mGeofenceRequestIntent = getGeofenceTransitionPendingIntent();
+        LocationServices.GeofencingApi.addGeofences(mGoogleApiCleint, geofences, mGeofenceRequestIntent);
+
+        //TODO: show message iniciando geofence
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    private boolean availableGoogleApiServies() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (ConnectionResult.SUCCESS == resultCode) {
+            Snackbar.make(fab, "", BaseTransientBottomBar.LENGTH_LONG).show();
+            return true;
+        }
+        Snackbar.make(fab, "Servicios de Google Play no disponibles", BaseTransientBottomBar.LENGTH_LONG).show();
+        return false;
+    }
+
+    private PendingIntent getGeofenceTransitionPendingIntent() {
+        Intent intent = new Intent(this, GeofenceTransitionIntentService.class);
+        return PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
     }
 }
