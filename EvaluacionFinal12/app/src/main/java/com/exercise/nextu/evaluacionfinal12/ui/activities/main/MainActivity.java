da73fa9d41eca1631faf9b4066c75ad89b10c5f5
Sergio Rojas <sart1991@gmail.com>
2017-11-08 21:58:26 -0500

    signed apk and interstitial banner working

--- EvaluacionFinal12/app/src/main/java/com/exercise/nextu/evaluacionfinal12/ui/activities/main/MainActivity.java
+++ EvaluacionFinal12/app/src/main/java/com/exercise/nextu/evaluacionfinal12/ui/activities/main/MainActivity.java
@@ -3,6 +3,7 @@ package com.exercise.nextu.evaluacionfinal12.ui.activities.main;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
+import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,6 +11,8 @@ import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
@@ -30,6 +33,10 @@ import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -39,6 +46,12 @@ import java.util.Set;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final long AD_TIME = 3000;
+    private InterstitialAd mInterstitialAd;
+    private CountDownTimer mCountDownTimer;
+    private boolean adInProgress;
+    private long mTimer;
 
     private MainViewModel viewModel;
 
@@ -54,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
         setUp();
+        startAd();
     }
 
     private void setUp() {
@@ -90,6 +104,11 @@ public class MainActivity extends AppCompatActivity {
         movementAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerMovement.setAdapter(movementAdapter);
         fab.setOnClickListener(onFabClick);
+        String interstitialAdKey = getString(R.string.interstitial_ad_key);
+        MobileAds.initialize(this, interstitialAdKey);
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId(interstitialAdKey);
+        mInterstitialAd.setAdListener(onAdAction);
     }
 
     private void setUpInitialData() {
@@ -138,6 +157,45 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    private AdListener onAdAction = new AdListener() {
+        @Override
+        public void onAdClosed() {
+            super.onAdClosed();
+            startAd();
+        }
+    };
+
+    private void createTimer(final long milliseconds) {
+        if (mCountDownTimer != null) {
+            mCountDownTimer.cancel();
+        }
+
+        final Snackbar snack = Snackbar.make(fab, "", Snackbar.LENGTH_INDEFINITE);
+
+        mCountDownTimer = new CountDownTimer(milliseconds, 50) {
+            @Override
+            public void onTick(long l) {
+                mTimer = l;
+                snack.setText(getText(R.string.mainActivity_snack_timer) + " " + (l/1000 + 1));
+                snack.show();
+            }
+
+            @Override
+            public void onFinish() {
+                adInProgress = false;
+                snack.dismiss();
+            }
+        };
+    }
+
+    private void showSnackBarInfo(int resMessage) {
+        showSnackBarInfo(getString(resMessage));
+    }
+
+    private void showSnackBarInfo(String message) {
+        showSnackBar(message, R.color.colorBlack);
+    }
+
     private void showSnackError(int resMessage) {
         showSnackError(getString(resMessage));
     }
@@ -150,5 +208,54 @@ public class MainActivity extends AppCompatActivity {
         Snackbar snack = Snackbar.make(fab, message, Snackbar.LENGTH_LONG);
         snack.getView().setBackgroundResource(resColor);
         snack.show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_options, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.mainOptions_ad:
+                showInterstitialAd();
+                return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (adInProgress) {
+
+        }
+    }
+
+    private void showInterstitialAd() {
+        if (mInterstitialAd != null && mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            showSnackBarInfo(R.string.mainActivity_snack_adNotLoaded);
+        }
+    }
+
+    private void startAd() {
+        if (!mInterstitialAd.isLoading() && !mInterstitialAd.isLoaded()) {
+            AdRequest adRequest = new AdRequest.Builder().build();
+            mInterstitialAd.loadAd(adRequest);
+        }
+        resumeAd(AD_TIME);
+    }
+
+    private void resumeAd(long milliseconds) {
+        adInProgress = true;
+        mTimer = milliseconds;
+        createTimer(milliseconds);
+        mCountDownTimer.start();
     }
 }
